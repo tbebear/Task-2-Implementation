da06ff5452b8faa31d5ab14bb23271da28a5608f
tbebear <aldisofyan2002@gmail.com>
2022-10-20 13:03:35 +0700

    Update No2.py

--- No2.py
+++ No2.py
@@ -1,23 +1,21 @@
-
-from datetime import datetime
 from datetime import time
 import string 
 
-currentime = datetime.now()
+currentime = time(13,0,0) #declare time now 
 star = time(8,0,0)
 end = time(17,0,0)
-Basefee : int
+Basefee = 0
 x : int
-Addfee : int
+Addfee = 0
 drop : string
 
 
-x = input(int("Welcome please input the computer-amount : "))
+x = int(input("Welcome please input the computer-amount : "))
 if (x == 1 or x == 2):
-    Basefee+= 50
+    Basefee += 50
     Addfee = 0
 elif (x >= 3 and x <= 10):
-    Basefee+= 50 
+    Basefee += 100 
     for i in range(0,x):
         Addfee += 10
 elif (x >10):
@@ -26,9 +24,8 @@ elif (x >10):
         Addfee += 10
 if (currentime < star and currentime > end):
     Basefee = Basefee * 2
-drop = input(string("Are you willing to drop off and pick up type (yes): "))
-if (drop == "yes"):
+drop = int(input("Are you willing to drop off and pick up type (1 if yes): "))
+if (drop == 1):
     Basefee = Basefee / 2
 
 print("Total fee : ",Basefee + Addfee)
-
